0b00e4837811a09153c9022de64c427258b55c9f
Zachary Schulz <zjschulz@gmail.com>
2019-09-21 00:41:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,14 +11,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.all? {|i| i.length < 4}
-  
-  all_odd = true
-  calls.each do |number|
-  if number.length <= 4 
-    all_odd = false
-  end
-end
+  calls.all? {|i| i.length <= 4}
 end
 
 def find_the_cheese# code an argument here
