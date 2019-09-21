0fe9b29c4831b850cf07a848f8c22df26dfce4de
Zachary Schulz <zjschulz@gmail.com>
2019-09-21 00:45:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? {|x| x.length <= 4}
+  calls.any? {|x| x.length >= 4}
 end
 
 def find_the_cheese# code an argument here
